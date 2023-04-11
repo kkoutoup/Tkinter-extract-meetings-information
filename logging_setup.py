89067c35d29e9809b34bf7533a84d99fbb79dead
kkoutoup <kkoutoup@gmail.com>
2023-04-11 20:34:06 +0100

    Refactored logging_setup.py

--- logging_setup.py
+++ logging_setup.py
@@ -1,5 +1,8 @@
 # Python modules
 import logging
 
+# format log
+format_log = "%(asctime)s | File: %(filename)s | Function: %(funcName)s | %(message)s"
+
 # set up logging
-logging.basicConfig(filename='meetings_report.log', format='%(asctime)s-%(levelname)s\n%(message)s', datefmt='%d/%m/%Y @ %H:%M:%S', filemode='w', level=logging.INFO)
+logging.basicConfig(filename = 'meetings_report.log', format = format_log, datefmt = '%d/%m/%Y @ %H:%M:%S', filemode = 'w', level = logging.INFO)
